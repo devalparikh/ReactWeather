a60976996cc3ba894c07e2b848fdc6ef86f9351b
Deval Parikh <devalpp@gmail.com>
2018-03-15 01:47:42 -0400

    init ReactWeather

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">React Weather</h1>
+    <h3 className="title-container__subtitle">By: Deval Parikh.</h3>
+  </div>
+);
+
+export default Titles;
